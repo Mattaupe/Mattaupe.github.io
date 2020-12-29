0878ecc1063fcd72e556f50fb438ed846fe71c78
Matthieu <73494255+Mattaupe@users.noreply.github.com>
2020-12-29 13:32:59 +0100

    1

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import * as tf from '@tensorflow/tfjs';
-
-async function Usemodel(img) {
+ 
+const model = await tf.loadLayerModel('https://mattaupe.github.io/models/model.json'); 
     
 
-    const model = await tf.loadLayerModel('https://mattaupe.github.io/models/model.json'); 
-    
-    const prediction = model.predict(img);   
-    return prediction;  
-}
