8330c2e4ff01ec5eff81cea20919a465deb72943
Matthieu <73494255+Mattaupe@users.noreply.github.com>
2021-01-02 17:14:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import * as tf from '@tensorflow/tfjs';
 
-const model = await tf.loadLayerModel('https://mattaupe.github.io/models/model.json'); 
+function predic(){
+ 
+    const model = await tf.loadLayerModel('https://mattaupe.github.io/models/model.json'); 
     
-const example = tf.fromPixels(webcamElement);  // for example
-const prediction = model.predict(example);  
+    const example = tf.fromPixels(webcamElement);  // for example
+    const prediction = model.predict(example);  
+    
+    console.log(prediction);
 
-console.log(prediction);
+}
+
+
 
